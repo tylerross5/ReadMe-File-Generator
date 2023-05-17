96a2ab45cabb6ee84d44b6fdcd281c5dc0ee9f20
Tyler <tbobross5@optimum.net>
2023-05-17 13:08:16 -0400

    added code

---
+++ Develop/README.md
@@ -0,0 +1,54 @@
+# Readme file generator
+  
+undefined
+
+## Description
+
+Creates readme files for users to help streamline the process of making readme files
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+undefined
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+
+npm i
+
+```
+
+## Usage
+
+undefined
+
+undefined
+  
+## Contributing
+
+undefined
+
+## Tests
+
+To run tests, run the following command:
+
+```
+npm test
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at tbobross5@optimum.net.
+You can find more of my work at [Tyler Ross](https://github.com/Tyler Ross/).
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,57 @@
-// TODO: Include packages needed for this application
+const fs= require('fs')
+const path= require('path')
+const inquirer= require('inquirer')
+const generateMarkdown= require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's name?",
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+      }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+      console.log('Generating README...');
+      writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+    });
+  }
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
